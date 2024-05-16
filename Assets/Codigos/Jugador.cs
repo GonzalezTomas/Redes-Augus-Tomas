70310fd9610912746190ab692f7e15e7aa759b84
ziinovv <augustocouture44@gmail.com>
2024-05-16 12:12:31 -0300

    jasja

--- Assets/Codigos/Jugador.cs
+++ Assets/Codigos/Jugador.cs
@@ -7,16 +7,25 @@ public class Jugador : NetworkBehaviour
 {
     public float speed;
     public float jumpForce;
+    public float danio = 20f;
     public Animator anim;
+    public LayerMask LayerDisapro;
 
     private Rigidbody rb;
     private float horizontalInput;
+    private bool _disparo;
+
+  //  [Networked, OnChangedRender(nameof(OnNetHealtChanged))]
+    void OnNetHealtChanged() => Debug.Log($"Life = {NetworkedHealth}");
+    private float NetworkedHealth { get; set; } = 100;
+
+   
 
     public override void Spawned()
     {
         rb = GetComponent<Rigidbody>();
 
-        if (!HasStateAuthority) return;
+        if (!HasStateAuthority) return;      
 
         Camera.main.GetComponent<Camara>()?.Target(transform);
     }
@@ -31,16 +40,68 @@ public class Jugador : NetworkBehaviour
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             // Aquí puedes agregar cualquier animación de salto que necesites
         }
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            _disparo = true;
+        }
+
     }
 
     public override void FixedUpdateNetwork()
     {
         base.FixedUpdateNetwork();
 
-        // Mover el jugador horizontalmente
+        transform.forward = Vector3.right * Mathf.Sign((horizontalInput));
+       
         Vector3 movement = Vector3.right * horizontalInput * Runner.DeltaTime * speed;
-        rb.MovePosition(rb.position + movement);
+       
+        rb.MovePosition(rb.position + movement);  
+
+        if(_disparo)
+        {
+            Disparo();
+
+           // Bala();
+
+            _disparo =false;
+        }
+
     }
+
+    void Disparo()
+    {
+        Debug.DrawRay(transform.position, transform.forward, Color.red, 2f);
+
+        if(Runner.GetPhysicsScene().Raycast(transform.position, transform.forward, out var raycastHit, LayerDisapro)) 
+        {
+            var jugador = raycastHit.transform.GetComponent<Jugador>();
+
+            jugador.RecibirDaño(danio);
+        }
+    }
+
+    void Bala()
+    {
+
+    }
+
+    [Rpc(RpcSources.All,RpcTargets.StateAuthority)]
+    public void RecibirDaño(float dmg)
+    {
+        NetworkedHealth -= dmg;
+
+        if (NetworkedHealth <= 0)
+        {
+            Muere();
+        }
+
+    }
+    
+    void Muere()
+    {
+        Runner.Despawn(Object);
+    }
+   
 
     private bool IsGrounded()
     {
