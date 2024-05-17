4e2b30a2fd55d6948b915fcf7c684bb6f050618b
GonzalezTomas <TomasG102@hotmail.com>
2024-05-17 17:23:25 -0300

    hola

--- Assets/Codigos/UI_Manager.cs
+++ Assets/Codigos/UI_Manager.cs
@@ -10,35 +10,26 @@ public class UI_Manager : MonoBehaviour
 
     public GameObject pantallaPerdiste;
     public GameObject pantallaGanaste;
-    public GameManager gameManager;
-
-    private void Start()
-    {
-        gameManager = FindObjectOfType<GameManager>();
-    }
 
     public void UpdatePlayerCount(int count)
     {
         playerCountText.text = count < 2 ? "Faltan más jugadores para empezar la partida" : "Jugadores en la partida: " + count;
     }
 
-    public void MostrarPantallaPerdiste(Jugador perdedor)
+    public void MostrarPantallaPerdiste()
     {
         pantallaPerdiste.SetActive(true);
-        // Puedes personalizar el mensaje de perdiste aquí si es necesario
     }
 
     public void MostrarPantallaGanaste(Jugador ganador)
     {
         pantallaGanaste.SetActive(true);
-        // Puedes personalizar el mensaje de ganador aquí si es necesario
+        // Aquí puedes personalizar el mensaje de ganador si es necesario
     }
 
     public void ReiniciarJuego()
     {
-        pantallaPerdiste.SetActive(false);
-        pantallaGanaste.SetActive(false);
-        gameManager.ReiniciarJuego();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
 
