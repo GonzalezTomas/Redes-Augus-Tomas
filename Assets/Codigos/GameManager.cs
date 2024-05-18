f49088b7f48271c2781600619f1ba3d500b44de8
GonzalezTomas <TomasG102@hotmail.com>
2024-05-17 22:30:04 -0300

    SFFSF

--- Assets/Codigos/GameManager.cs
+++ Assets/Codigos/GameManager.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 using Fusion;
 using System.Runtime.CompilerServices;
 
@@ -96,7 +95,7 @@ public class GameManager : NetworkBehaviour
     }
 
 
-    
+
 
     public void JugadorMuerto(Jugador jugador)
     {
